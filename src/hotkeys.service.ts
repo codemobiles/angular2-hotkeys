af19d2211749bdeec4b353c26c362b9fcdb427d0
Codemobiles00 <codemobiles@Codemobiles00s-iMac.local>
2020-10-27 10:44:06 +0700

    fix bug

--- src/hotkeys.service.ts
+++ src/hotkeys.service.ts
@@ -2,13 +2,13 @@ import { HotkeyOptions, IHotkeyOptions } from './hotkey.options'
 import { Subject } from 'rxjs'
 import { Inject, Injectable } from '@angular/core'
 import { Hotkey } from './hotkey.model'
-import 'mousetrap'
+import * as Mousetrap from 'mousetrap'
 
 @Injectable()
 export class HotkeysService {
   hotkeys: Hotkey[] = []
   pausedHotkeys: Hotkey[] = []
-  mousetrap: MousetrapInstance
+  mousetrap: Mousetrap.MousetrapInstance
   cheatSheetToggle: Subject<any> = new Subject()
 
   private _preventIn = ['INPUT', 'SELECT', 'TEXTAREA']
